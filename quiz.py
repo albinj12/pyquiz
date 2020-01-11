796fb970da65d5e9575bdaea7d96a0199e01d594
albin-jose <albinavirappatt@gmail.com>
2020-01-11 13:22:57 +0530

    added more subjects and minor code refractor

--- quiz.py
+++ quiz.py
@@ -1,13 +1,25 @@
 import csv
 
-print("Welcome to pyquiz")
-print("Available streams are maths, science and GK.\nAll streams have 10 questions in total.\nEvery question have one mark each\n")
+studName = input("Please enter your name : ")
+print("\nHey "+studName+", Welcome to pyquiz")
+print("\nAvailable streams are maths, science and GK.\nAll streams have 10 questions in total.\nEvery question have one mark each\n")
 
+stream = ["maths","science","gk"]
 score = 0
 
-#quizStream = input("Select quiz subject : ")
 
-with open('maths.csv','r') as csvFile:
+
+while True :
+    quizStream = input("Select quiz subject : ")
+    if quizStream.lower() not in stream:
+        print("\nYou had entered a wrong choice.Please choose Maths, Science or GK")
+        continue
+    else:
+        break
+    
+filename = quizStream.lower()+".csv"
+
+with open(filename,'r') as csvFile:
     csvReader = csv.reader(csvFile)
     for row in csvReader:
         ans = input(row[0]+" : ")
